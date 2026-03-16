2fba72a207130dd67592f2ea9918fd76749d5709
Collinsdaberechukwu <collinsdaberechi20@gmail.com>
2026-03-16 14:20:38 +0100

    Layered structure initial commit

---
+++ src/main/java/com/collins/Skaet_wallet_system/SkaetWalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.collins.Skaet_wallet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaetWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaetWalletSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collins/Skaet_wallet_system/SkaetWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collins.Skaet_wallet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaetWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
